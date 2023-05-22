73255420b052257370b9721610ff76d240af6608
Mark Boguslavski <boguslavski.mark@gmail.com>
2023-05-22 16:47:47 +0300

    1

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import { Product } from "../../app/models/products"
 import ProductList from "./ProductList";
+import agent from "../../app/api/agent";
 
 export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
   
     //gets the products from the api
     useEffect(() => { 
-      fetch('http://localhost:5195/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
+        agent.Catalog.list().then(products => setProducts(products))
     }, [])
   
     return (
